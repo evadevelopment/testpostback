b7a417d0d7661a5c1d4e78f211667eb55d47847a
evadevelopment <119099361+evadevelopment@users.noreply.github.com>
2025-04-01 15:06:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
+import os
+from jose import jwt
+from fastapi import FastAPI, Form, HTTPException
 from typing import Optional
-
-from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
+secret_key = os.environ.get("ULTRA_SECRET_KEY")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/test")
+async def handle_test():
+    return {"message": "TEST OK"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/")
+async def handle_postback(
+    status: Optional[str] = Form(None),
+    invoice_id: Optional[str] = Form(None),
+    amount_crypto: Optional[str] = Form(None),
+    currency: Optional[str] = Form(None),
+    order_id: Optional[str] = Form(None),
+    token: Optional[str] = Form(None)
+):
+    try:
+        if token:
+            decoded = jwt.decode(token, secret_key, algorithms=['HS256'])
+        else:
+            raise HTTPException(status_code=400, detail="Token is required")
+
+        print(f"Payment status: {status}\n" \
+                   f"Invoice ID: {invoice_id}\n" \
+                   f"Amount in crypto: {amount_crypto} {currency}\n" \
+                   f"Order ID: {order_id}\n" \
+                   f"Decoded: {decoded}\n")
+
+        return {"message": "Postback received"}
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
